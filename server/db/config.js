264ef3434a5a35c3e6998aae02d4df5291318c33
Beta <me@pbeta.me>
2019-04-10 09:54:17 +0800

    done:修复过期逻辑,添加接口登录验证

--- server/db/config.js
+++ server/db/config.js
@@ -1,8 +1,8 @@
 export default {
-  mongo: 'mongodb://test:test12345@db.pup.pub:27017/wunderlist',
+  mongo: 'mongodb://test:test12345@hk.pup.pub:27017/wunderlist',
   redis: {
     get host() {
-      return 'db.pup.pub'
+      return 'hk.pup.pub'
     },
     get port() {
       return 6379
